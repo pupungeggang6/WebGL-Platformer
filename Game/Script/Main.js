721fb2670f5a332c3a67c2f66a7f8c5962bd41a4
pupungeggang6 <pupungeggang6@gmail.com>
2025-02-11 16:53:22 +0900

    Start

---
+++ Game/Script/Main.js
@@ -0,0 +1,13 @@
+window.onload = main
+window.onerror = errorHandle
+window.oncontextmenu = rightClick
+
+function main() {
+}
+
+function errorHandle() {
+}
+
+function rightClick() {
+    return false
+}
---
+++ Game/Script/Var.js
@@ -0,0 +1,13 @@
+let canvas
+let gl
+let canvasUI
+let context
+
+let gameLoop
+let frameCurrent
+let framePrevious
+let delta
+
+let scene = 'Title'
+let state = 'Start'
+let menu = false
